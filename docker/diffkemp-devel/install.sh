57d28c71601a2b0df66979c4cd5a085a6a35f278
Lukas Petr <lukas.petr.dev@gmail.com>
2023-10-08 09:33:37 +0200

    Docker-dev: add install of requirements.txt
    `requirements.txt` can change over time, installing requirements
    for every launch of docker-devel.

--- docker/diffkemp-devel/install.sh
+++ docker/diffkemp-devel/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 /switch-llvm.sh $LLVM_VERSION
+python3 -m pip install -r ${DIFFKEMP_DIR}/requirements.txt --ignore-installed
 mkdir -p $SIMPLL_BUILD_DIR
 cd $SIMPLL_BUILD_DIR
-cmake $DIFFKEMP_DIR -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_VIEWER=OFF
+cmake $DIFFKEMP_DIR -GNinja -DVENDOR_GTEST=On -DBUILD_VIEWER=OFF
 ninja -j4
 cd -
 python3 -m pip install -e $DIFFKEMP_DIR
