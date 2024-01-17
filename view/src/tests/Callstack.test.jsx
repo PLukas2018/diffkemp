100673bac92ce06b532d396127e8588cc808b386
Lukas Petr <lukas.petr.dev@gmail.com>
2024-01-17 11:29:59 +0100

    Viewer: Add tests for macro-function diffs
    An example of a macro-function difference was moved to
    `example.js` file so it can be reused in multiple test files.
    Following tests were added:
    - testing appearance (calls alignment) for a call stack with
      the macro-function difference,
    - testing call stack default reaction (which calls are selected)
      and reaction when selecting macro x function in the call stack
      of the macro-function difference.

--- view/src/tests/Callstack.test.jsx
+++ view/src/tests/Callstack.test.jsx
@@ -7,6 +7,7 @@ import {
 import '@testing-library/jest-dom';
 
 import Callstack from '../components/Callstack';
+import { MacroFunctionDifference } from './examples';
 
 describe('simple callstack visualisation test', () => {
   const compFunName = '__put_task_struct';
@@ -84,15 +85,12 @@ describe('complex callstack visualisation test', () => {
 });
 
 describe("callstack with 'different' differing functions", () => {
-  const compFunName = 'down_write';
-  const oldCallStack = [{ name: 'get_task_struct (macro)' }];
-  const newCallStack = [{ name: 'get_task_struct' }];
   test('all function names should be visible', () => {
     render(
       <Callstack
-        compFunName={compFunName}
-        oldCallStack={oldCallStack}
-        newCallStack={newCallStack}
+        compFunName={MacroFunctionDifference.function}
+        oldCallStack={MacroFunctionDifference.diffs[0]['old-callstack']}
+        newCallStack={MacroFunctionDifference.diffs[0]['new-callstack']}
         definitions={{}}
         onSelect={() => {}}
       />,
---
+++ view/src/tests/examples.js
@@ -0,0 +1,20 @@
+// Examples of differences (real or created) for testing functionality of the viewer.
+
+/* eslint import/prefer-default-export: "off" */
+
+export const MacroFunctionDifference = {
+  // compared function
+  function: 'down_write',
+  diffs: [{
+    // differing function
+    function: 'get_task_struct',
+    'old-callstack': [{ name: 'get_task_struct (macro)' }],
+    'new-callstack': [{ name: 'get_task_struct' }],
+  }],
+  // Expected call stack visualisation:
+  // ----------------------------------------------------------------
+  // | down_write                                                   |
+  // |--------------------------------------------------------------|
+  // | get_task_struct (macro)         | get_task_struct            |
+  // ----------------------------------------------------------------
+};
